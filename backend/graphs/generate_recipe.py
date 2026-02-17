4be5151d7f5058a14f150e148329ff9c51fb7294
Alan Cho <alancho397@gmail.com>
2026-02-17 16:29:41 -0600

    feat: add generate_recipe graph

---
+++ backend/graphs/generate_recipe.py
@@ -0,0 +1,22 @@
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import SystemMessage, HumanMessage
+
+from backend.schemas.app_state import AppState
+from tools.web_search import web_search
+
+tools = [web_search]
+llm = ChatAnthropic(model="claude-sonnet-4-5-20250929").bind_tools(tools)
+
+
+def generate_recipe(state: AppState) -> AppState:
+    if not state.get("messages"):
+        system_prompt = ""
+        messages = [
+            SystemMessage(content=system_prompt),
+            HumanMessage(content=state["query"]),
+        ]
+    else:
+        messages = state["messages"]
+
+    response = llm.invoke(messages)
+    return {"messages": [response]}
