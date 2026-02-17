de25cd075414bee614cb883d237ce1bed13ba089
Alan Cho <alancho397@gmail.com>
2026-02-17 16:23:10 -0600

    feat(wip): state schema

---
+++ backend/schemas/app_state.py
@@ -0,0 +1,16 @@
+from typing import Annotated, TypedDict, Literal, Optional
+
+from langchain_core.messages import AnyMessage
+from langgraph.graph.message import add_messages
+
+
+class AppState(TypedDict):
+    query: str
+    query_intent: Optional[Literal["recipe_request", "what_can_i_make", "general"]]
+
+    is_cooking_related: bool
+
+    messages: Annotated[list[AnyMessage], add_messages]
+
+    available_cookware: list[str]
+    response: str
